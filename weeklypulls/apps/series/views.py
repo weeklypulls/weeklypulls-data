20528491115885c8e1dbb46f691853fb70e4f6f8
Robert Kuykendall <robert@rkuykendall.com>
2017-12-28 11:54:26 -0500

    Make pull list writable

--- weeklypulls/apps/series/views.py
+++ weeklypulls/apps/series/views.py
@@ -1,8 +1,13 @@
 from weeklypulls.apps.series.models import Series
 from rest_framework import routers, serializers, viewsets
 
+from weeklypulls.apps.pull_lists.models import PullList
+
 
 class SeriesSerializer(serializers.HyperlinkedModelSerializer):
+    pull_list_id = serializers.PrimaryKeyRelatedField(
+        source='pull_list', queryset=PullList.objects.all())
+
     class Meta:
         model = Series
         fields = ('id', 'series_id', 'read', 'skipped', 'pull_list_id', )
