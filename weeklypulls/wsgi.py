036ae222dd7cab1d91f141872516a8841dc8aa14
Robert Kuykendall <robert@rkuykendall.com>
2026-08-22 19:58:19 -0500

    Log a startup line to verify app logging reaches Heroku

--- weeklypulls/wsgi.py
+++ weeklypulls/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weeklypulls.settings")
 
 application = get_wsgi_application()
+
+logging.getLogger(__name__).info("weeklypulls: logging configured, worker booted")
